ac2cfe2fc893e04390395bf25728ae88689391e6
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-07 18:55:22 +0530

    Updates

---
+++ precache-manifest.c4727d3e5fa28eb5aa55446035b64f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5e30ce234646350598290dd85451d4",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "fa00d248aea9e17275aa",
+    "url": "/drum-kit/static/css/main.1a41b428.chunk.css"
+  },
+  {
+    "revision": "91fc3ec1dd907887b1eb",
+    "url": "/drum-kit/static/js/2.da75ad52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-kit/static/js/2.da75ad52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa00d248aea9e17275aa",
+    "url": "/drum-kit/static/js/main.b68f165b.chunk.js"
+  },
+  {
+    "revision": "ebce98943a4abe806798",
+    "url": "/drum-kit/static/js/runtime-main.dbe720cd.js"
+  }
+]);
