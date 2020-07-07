d347ec9999f75dc49c6222ad1097c1fa90e380d5
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-07 18:48:04 +0530

    Added Click Functionality

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const KitDetails = [
+  { keyCode: 'Q',
+    url:'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    name:'Heater 1'},
+  { keyCode: 'W',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    name:'Kick n Hat' },
+  { keyCode: 'E',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    name: 'Cev_H2' },
+  { keyCode: 'A',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+    name: 'Chord 1' },
+  { keyCode: 'S',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+    name: 'Chord 2' },
+  { keyCode: 'D',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+    name: 'Chord 3' },
+  { keyCode: 'Z',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+    name: 'Bld H1' },
+  { keyCode: 'X',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+    name: 'Punchy Kick' },
+  { keyCode: 'C',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+    name: 'Break Snare' }
+]
+
+class DrumPad extends Component {
+  constructor(props) {
+    super(props)
+  
+  }
+  componentDidMount() {
+    document.addEventListener("keydown",this.HandleKeyPress)
+  }
+
+  HandleKeyPress =(e) => {
+    let pressedKey = e != null ? e.key.toUpperCase() : e
+    this.PlayAudio(pressedKey)
+  }
+
+  PlayAudio =(e) => { 
+    
+    let audioObj = document.getElementById(e)
+    if(audioObj!=null) {
+      audioObj.currentTime =0;
+      audioObj.play()
+      setTimeout(audioObj.play(),100);
+    }
+    
+    console.log(audioObj)
+  }
+
+  
+  
+  render() {
+    let myKey = this.props.keyVal;
+
+    return (
+      <button value= {myKey} className="push-box" onClick={(e) =>this.PlayAudio(e.target.value)}>
+      {myKey}
+        <audio className="clip" id={myKey} src={this.props.URL} />
+      </button>
+    )
+  }
+}
+
+
+class PadLine extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  showItem = () => {
+    let startNum = this.props.startNum
+    let struc=[]
+      for (let i = 0; i <= 2; i++) {      
+          struc[i]=<DrumPad keyVal={KitDetails[startNum+i].keyCode} URL={KitDetails[startNum+i].url} padName={KitDetails[startNum+i].name} />
+      }
+      return struc
+  }
+  render() {
+
+    return (
+      <div className="kit-line">
+        {this.showItem()}
+      </div>
+    )
+  }
+
+}
+
+class App extends Component {
+constructor(props) {
+  super(props)
+}
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container" id="drum-machine">
+          <PadLine startNum={0} />
+          <PadLine startNum={3} />
+          <PadLine startNum={6} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
